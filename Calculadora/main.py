7e5681f2102d5c1a951d8df5bf6ec7c889292629
MIGUEL_ARIAS <migariast@gmail.com>
2024-10-01 22:02:54 -0500

    First commituwu

---
+++ Calculadora/main.py
@@ -0,0 +1,22 @@
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División por cero."
+    
+def calculadora():
+    while True:
+        print("\nOpciones de Operación:")
+        print("1. Sumar")
+        print("2. Restar")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("5. Salir")
+
+        opcion = input("Selecciona una opción (1/2/3/4/5): ")
